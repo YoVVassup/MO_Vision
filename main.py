1da4e6957f22ab07a5c8647d879424f6ceeb181b
yowassup <yowassup1992@gmail.com>
2022-05-16 13:19:25 +0300

    'Add Version + GlobalMO files'

--- main.py
+++ main.py
@@ -19,25 +19,35 @@ def remove():
         os.mkdir(f'{cwd}\\INI\\')
     if os.path.exists(BattleClient_path):
         os.remove(BattleClient_path)
+    if os.path.exists(MentalOmegaMaps_path):
+        os.remove(MentalOmegaMaps_path)
     if os.path.exists(artmo_path):
         os.remove(artmo_path)
     if os.path.exists(fan_art_path):
         os.remove(fan_art_path)
-    BattleClientOriginal = zipfile.ZipFile(f'{cwd}\\Vision_ini.zip')
-    BattleClientOriginal.extract('INI/BattleClient.ini', f'{cwd}')
-    BattleClientOriginal.close()
+    if os.path.exists(soundmo_path):
+        os.remove(soundmo_path)
+    OriginalFiles = zipfile.ZipFile(f'{cwd}\\Vision_ini.zip')
+    OriginalFiles.extract('INI/BattleClient.ini', f'{cwd}')
+    OriginalFiles.extract('INI/MentalOmegaMaps.ini', f'{cwd}')
+    OriginalFiles.close()
 
 
 cwd = resource_path(Path.cwd())
 BattleClient_path = f'{cwd}\\INI\\BattleClient.ini'
+MentalOmegaMaps_path = f'{cwd}\\INI\\MentalOmegaMaps.ini'
 artmo_path = f'{cwd}\\artmo.ini'
 fan_art_path = f'{cwd}\\fan_art.ini'
+soundmo_path = f'{cwd}\\soundmo.ini'
+
 remove()  # очистка к первоначальному состоянию
 
 Vision_zip = zipfile.ZipFile(f'{cwd}\\Vision_ini.zip')
 Vision_zip.extract('BattleClient.ini', f'{cwd}\\INI\\')
+Vision_zip.extract('MentalOmegaMaps.ini', f'{cwd}\\INI\\')
 Vision_zip.extract('artmo.ini', f'{cwd}')
 Vision_zip.extract('fan_art.ini', f'{cwd}')
+Vision_zip.extract('soundmo.ini', f'{cwd}')
 Vision_zip.close()
 
 if os.path.exists(f'{cwd}\\MentalOmegaClient.exe'):  # проверка на существование exe
---
+++ version.py
@@ -0,0 +1,12 @@
+import pyinstaller_versionfile
+
+pyinstaller_versionfile.create_versionfile(
+    output_file="versionfile.txt",
+    version="1.0.3",
+    company_name="YoWassup Inc",
+    file_description="MO Vision",
+    internal_name="MO Vision",
+    legal_copyright="© YoWassup Inc. All rights reserved.",
+    original_filename="MO Vision.exe",
+    product_name="MO Vision"
+)
